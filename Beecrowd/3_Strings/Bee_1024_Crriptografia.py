23bae04f7f6b502aeaf9aa05f54892cffa9a0033
Felipe Ferraz <felipeferraz2402@gmail.com>
2023-12-25 23:47:55 -0300

    Beecrowd 1024

--- Beecrowd/3_Strings/Bee_1024_Crriptografia.py
+++ Beecrowd/3_Strings/Bee_1024_Crriptografia.py
@@ -1,3 +1,23 @@
-texto = 'Texto #3'
+times = int(input())
+for x in range(times):
+    texto = input()
+    frase = []
 
-print(texto)
+    for i in range(len(texto)):
+        simbolo = texto[i]
+        if(simbolo.isalpha()):
+            simbolo = chr(ord(texto[i]) + 3)
+        frase.append(simbolo)
+    texto = ''.join(frase)
+
+    texto = texto[::-1]
+    frase = []
+
+    for i in range(len(texto)):
+        simbolo = texto[i]
+        if(i >= len(texto)//2):
+            simbolo = chr(ord(texto[i]) - 1)
+        frase.append(simbolo)
+    texto = ''.join(frase)
+
+    print(texto)
