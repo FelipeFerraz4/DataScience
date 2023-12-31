60ea2a201ad0d0f4a32d59f55bfb204a966aa9a9
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-12-31 17:25:48 -0300

    Beecrowd 2003

---
+++ Beecrowd/1_Iniciante/Bee_2003_Domingo_de_manha.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+      tempo = input().replace(':', ' ').split()
+      hora = int(tempo[0])
+      minutos = int(tempo[1])
+      resposata = 0
+      if hora == 7:
+          resposata += minutos
+      elif hora == 8:
+          resposata += 60 + minutos
+      elif hora == 9:
+          resposata +=  120 + minutos  
+      print(f'Atraso maximo: {resposata}')
+    except EOFError:
+      break
+    
